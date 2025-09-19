d80b83d01f7501dc9a9ac618e192e566a9972803
sissokocheick* <sissokocheickahmed14@gmail.com>
2025-09-19 00:42:47 +0000

    fix: add netlify redirect rule

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/somnia-stream-app/',
+  base: '/',
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          ethers: ['ethers']
-        }
-      }
-    }
-  },
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer',
-      process: 'process/browser',
-      stream: 'stream-browserify',
-      util: 'util'
-    }
-  },
-  optimizeDeps: {
-    include: ['buffer', 'process']
+    assetsDir: 'assets',
+    sourcemap: false
   }
 })
